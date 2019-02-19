6c4a2a2e48e39830083b2db5322e5785c54d5e68
beveloper <bkrofegha@mail.com>
2019-02-19 03:35:01 -0800

    adding navbar files

---
+++ src/Components/Navbar/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Header.css'
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <header className='header'>
+                    <nav>
+                        <ul>
+                            <li>kklzkcl</li>
+                            <li className='feeds'>#</li>
+                            <li>klskdlkd</li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </nav>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
